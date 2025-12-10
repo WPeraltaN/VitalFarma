ac1a3cd4fd41ae23ab4fffeb0e6204e3f7223e42
Winnar <winnarperalta3@gmail.com>
2025-12-10 12:31:50 -0400

    Estilos para el header de la parte del cliente

--- app/routes/main.py
+++ app/routes/main.py
@@ -3,9 +3,9 @@ from app import views
 urlpatterns = [
     # Pantallas de CLIENTE
     path('',views.inicio, name='inicio'),
-    path('/productos',views.productos, name='productos'),
+    path('productos/',views.productos, name='productos'),
 
     # Pantallas de AUTENTIFICACION
-    path('/login',views.login, name='login'),
-    path('/register',views.render, name='register')
+    path('login/',views.login, name='login'),
+    path('register/',views.render, name='register')
 ]
